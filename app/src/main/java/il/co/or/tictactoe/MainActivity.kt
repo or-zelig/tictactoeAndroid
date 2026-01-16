0dc9667f28fc170a887990aaa33424b4c657758c
97254 <orzelig6@gmail.com>
2026-01-16 19:18:03 +0200

    Switch MainActivity from Compose to XML layout

--- app/src/main/java/il/co/or/tictactoe/MainActivity.kt
+++ app/src/main/java/il/co/or/tictactoe/MainActivity.kt
@@ -15,18 +15,9 @@ import il.co.or.tictactoe.ui.theme.TictactoeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TictactoeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
+
 }
 
 @Composable
